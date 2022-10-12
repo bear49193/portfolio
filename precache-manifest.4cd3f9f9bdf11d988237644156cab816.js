db388e957355b1795787afa3e7ca3463da9157c0
bear49193 <rbeer199525@gmail.com>
2022-10-13 04:09:59 +0800

    ss

---
+++ precache-manifest.4cd3f9f9bdf11d988237644156cab816.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d671836f13e54b440e8df081d57e6486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79ec30bdcfcd517835a6",
+    "url": "/static/css/main.fb31a9f6.chunk.css"
+  },
+  {
+    "revision": "b5f221d63b2ac0d1ce4a",
+    "url": "/static/js/2.146c7337.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.146c7337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ec30bdcfcd517835a6",
+    "url": "/static/js/main.83a154c9.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "f3afea6e3784d78af14654fce4a31e19",
+    "url": "/static/media/code.f3afea6e.svg"
+  },
+  {
+    "revision": "8f53edd1344877333c4e84527f3b0af3",
+    "url": "/static/media/hexlogo.8f53edd1.svg"
+  },
+  {
+    "revision": "50baad3bf195eff69067bc20fe2b1088",
+    "url": "/static/media/plate.50baad3b.svg"
+  },
+  {
+    "revision": "9542bf91b8920b4d4b706034b795c38e",
+    "url": "/static/media/plato.9542bf91.svg"
+  }
+]);
