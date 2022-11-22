f42fe3009134d3715526f7c98476b6ca0ec982aa
developer <codedreams9873@gmail.com>
2022-11-22 16:46:20 +0800

    changed

---
+++ precache-manifest.7270878c8ce974d445b1ce39d5f0f4c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9411f45be59b67612176563eb7200618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3795b65b747e26b79afb",
+    "url": "/static/css/main.150b8ac9.chunk.css"
+  },
+  {
+    "revision": "b84275d3f76efa22e854",
+    "url": "/static/js/2.7c2ddf1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7c2ddf1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3795b65b747e26b79afb",
+    "url": "/static/js/main.ce91d60a.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "bab88693a251feace0fc7c22aebc516f",
+    "url": "/static/media/computer-desk.bab88693.svg"
+  },
+  {
+    "revision": "50baad3bf195eff69067bc20fe2b1088",
+    "url": "/static/media/plate.50baad3b.svg"
+  },
+  {
+    "revision": "9542bf91b8920b4d4b706034b795c38e",
+    "url": "/static/media/plato.9542bf91.svg"
+  }
+]);
